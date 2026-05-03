2ad229903f2cc3b6ee29973bfcc518e00485e3f7
Marek Dulowski <dulowski.marek@gmail.com>
2026-05-03 20:46:03 +0200

    Add Serialize trait to configs

--- src/config.rs
+++ src/config.rs
@@ -1,15 +1,15 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::collections::HashMap;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Config {
     pub output: String,
     #[serde(default)]
     pub modules: Vec<ModuleConfig>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct ModuleConfig {
     pub path: String,
     #[serde(default)]
